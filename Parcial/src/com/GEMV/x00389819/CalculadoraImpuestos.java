02db223eb0706bb53d038a4457a33223b92549dc
SahiK411 <00389819@uca.edu.sv>
2020-04-17 11:59:05 -0600

    Added: Final Class and Main functionality

---
+++ Parcial/src/com/GEMV/x00389819/CalculadoraImpuestos.java
@@ -0,0 +1,66 @@
+package com.GEMV.x00389819;
+
+public class CalculadoraImpuestos{
+    //Attributes
+    private static double totalRenta = 0;
+    private static double totalISSS = 0;
+    private static double totalAFP = 0;
+
+    //Methods
+    private CalculadoraImpuestos(){ }
+
+    public static double calcularPago(Empleado check) throws Exception {
+        if(check.salario <= 0){
+            throw new Exception();
+        }
+
+        double pago;
+        if(check instanceof ServicioProfesional){
+            double Renta = (0.1 * check.salario);
+            Renta = (double) Math.round(Renta * 100d) / 100d; //Para mantener 2 decimales
+            totalRenta += Renta;
+            pago = check.salario - Renta;
+            pago = (double) Math.round(pago * 100d) / 100d;
+            return pago;
+        }
+        else if(check instanceof PlazaFija){
+            double AFP = 0.0625d * check.salario;
+            AFP = (double) Math.round(AFP * 100d) / 100d;
+            totalAFP += AFP;
+
+            double ISSS = 0.03d * check.salario;
+            ISSS = (double) Math.round(ISSS * 100d) / 100d;
+            totalISSS += ISSS;
+
+            double Restante = check.salario - AFP - ISSS;
+            double Renta;
+            if(Restante > 0.01d && Restante <= 472.00d){
+                Renta = 0d;
+            }
+            else if(Restante <= 895.24d){
+                Renta = 0.1d * (Restante - 472d) + 17.67d;
+            }
+            else if(Restante <= 2038.10){
+                Renta = 0.2d * (Restante - 895.24d) + 60;
+            }
+            else{
+                Renta = 0.3d * (Restante - 2038.10d) + 288.57d;
+            }
+            Renta = (double) Math.round(Renta * 100d) / 100d;
+            totalRenta += Renta;
+
+            pago = Restante - Renta;
+            pago = (double) Math.round(pago * 100d) / 100d;
+            return pago;
+        }
+        else{
+            throw new IllegalArgumentException("Error de Clase!");
+        }
+    }
+
+    public static String mostrarTotales(){
+        return "Totales: \n" + "Renta: " + totalRenta + "\n" +
+                "ISSS: " + totalISSS + "\n" +
+                "AFP: " + totalAFP + "\n";
+    }
+}
--- Parcial/src/com/GEMV/x00389819/Main.java
+++ Parcial/src/com/GEMV/x00389819/Main.java
@@ -3,7 +3,46 @@ package com.GEMV.x00389819;
 public class Main {
 
     public static void main(String[] args) {
-        
+        String tName, tPost;
+        double tSalary;
+        int tVar;
+
+        try{
+            System.out.println(CalculadoraImpuestos.mostrarTotales());
+            tName = "William";
+            tPost = "Supervisor de Ventas";
+            tSalary = 579.00d;
+            tVar = 78985527;
+
+            PlazaFija empleado = new PlazaFija(tName, tPost, tSalary, tVar);
+            System.out.println("Empleado: " + empleado.getNombre() + "\n" +
+                    "Puesto: " + empleado.getPuesto() + "\n" +
+                    "Numero de Oficina: " + empleado.getExtension() + "\n" +
+                    "Salario antes de Impuestos: " + empleado.getSalario() + "\n" +
+                    "Salario: " + CalculadoraImpuestos.calcularPago(empleado));
+            System.out.println(CalculadoraImpuestos.mostrarTotales());
+
+            tName = "Bill";
+            tPost = "Vendedor";
+            tSalary = 420.00d;
+            tVar = 4;
+
+            ServicioProfesional contratador = new ServicioProfesional(tName, tPost, tSalary, tVar);
+            System.out.println("Empleado: " + contratador.getNombre() + "\n" +
+                    "Puesto: " + contratador.getPuesto() + "\n" +
+                    "Meses de Contrato: " + contratador.getMeses() + "\n" +
+                    "Salario antes de Impuestos: " + contratador.getSalario() + "\n" +
+                    "Salario: " + CalculadoraImpuestos.calcularPago(contratador));
+            System.out.println(CalculadoraImpuestos.mostrarTotales());
+        }
+        catch (Exception e){
+            if(e instanceof IllegalArgumentException){
+                System.out.println(e.getMessage());
+            }
+            else{
+                System.out.println("Se produjo un error!");
+            }
+        }
     }
 
 }
